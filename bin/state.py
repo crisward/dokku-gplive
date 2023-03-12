77898576ee46221a271128ca9e2db0331df7ae5e
Cris Ward <cris@duodesign.co.uk>
2023-03-12 01:34:47 +0000

    syntax

--- bin/state.py
+++ bin/state.py
@@ -139,7 +139,8 @@ def getServices():
   return services
   
 def fileList(dir):
-  if !os.path.exists(dir) return []
+  if !os.path.exists(dir):
+    return []
   stream = os.popen('ls -a '+dir+' | cat')
   output = stream.read().strip().split("\n")
   return [x for x in output if x.startswith(".")==False]
